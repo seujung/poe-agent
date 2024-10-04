f268b0b5e6da0295c8228e8da7d5afa67928a15a
seujung <digit82@gmail.com>
2024-10-04 21:20:55 +0900

    add: update packages

---
+++ echobot.py
@@ -0,0 +1,37 @@
+"""
+
+Sample bot that echoes back messages.
+
+This is the simplest possible bot and a great place to start if you want to build your own bot.
+
+"""
+
+from __future__ import annotations
+
+from typing import AsyncIterable
+
+import fastapi_poe as fp
+from modal import App, Image, asgi_app
+
+
+class EchoBot(fp.PoeBot):
+    async def get_response(
+        self, request: fp.QueryRequest
+    ) -> AsyncIterable[fp.PartialResponse]:
+        last_message = request.query[-1].content
+        yield fp.PartialResponse(text=last_message)
+
+
+REQUIREMENTS = ["fastapi-poe==0.0.48"]
+image = Image.debian_slim().pip_install(*REQUIREMENTS)
+app = App("echobot-poe")
+
+
+@app.function(image=image)
+@asgi_app()
+def fastapi_app():
+    bot = EchoBot()
+    # see https://creator.poe.com/docs/quick-start#configuring-the-access-credentials
+    # app = fp.make_app(bot, access_key=<YOUR_ACCESS_KEY>, bot_name=<YOUR_BOT_NAME>)
+    app = fp.make_app(bot, allow_without_key=True)
+    return app
